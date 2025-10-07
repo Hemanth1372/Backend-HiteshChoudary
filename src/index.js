48b1beaf65fefc66f0054a4f10010cdd23b114da
Hemanth1372 <hemanthguntuku18@gmail.com>
2025-10-08 00:16:07 +0530

    Succesfully connected to DB

---
+++ src/index.js
@@ -0,0 +1,34 @@
+// Even we write this it runs absolutely fine but
+// it makes the code inconsistent since it is require
+/*
+require("dotenv").config({ path: `./env` });
+*/
+
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: `./env`,
+});
+
+connectDB();
+
+// import express from "express";
+
+// const app = express();
+// async () => {
+//   try {
+//     await mongoose.connect(`${process.env.DATABASE_UR}/${DB_NAME}`);
+//     app.on("error", (error) => {
+//       console.log("Error: ", error);
+//       throw error;
+//     });
+
+//     app.listen(process.env.PORT, () => {
+//       console.log(`Server is listening on port ${process.env.PORT}`);
+//     });
+//   } catch (error) {
+//     console.log("ERROR : ", error);
+//     throw error;
+//   }
+// };
